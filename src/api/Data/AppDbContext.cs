b19dfdc5276c0a43992f5af3cc9bdc38407aa44a
Emerson J Santos <57603005+emersonv25@users.noreply.github.com>
2021-10-06 12:04:11 -0300

    create appdbcontext

---
+++ src/api/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+using api.Models.EntityModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Installment> Installments { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<Installment>().HasOne(i => i.Transaction).WithMany(t => t.Installments);
+            InstallmentMap.Map(modelBuilder.Entity<Installment>());
+            TransactionMap.Map(modelBuilder.Entity<Transaction>());
+
+        }
+
+    }
+}
